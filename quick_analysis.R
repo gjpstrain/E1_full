8bf5db99a81be0126309d7590939acfb93682e30
Andrew Stewart <drandrewjstewart@gmail.com>
2022-02-18 13:39:43 +0000

    add quick ANOVA

--- quick_analysis.R
+++ quick_analysis.R
@@ -4,35 +4,50 @@ library(emmeans)
 
 my_data <- read_csv("pav_data.csv")
 
-my_data %>% 
-  filter(my_rs !=0 & my_rs != 1) %>%
-  ggplot(aes(x = slider.response, y = my_rs)) +
-  geom_point()
+# separate the plots_with_labels column into 3 - item, size (3 levels), and present (2 levels)
 
-my_data %>%
-  mutate(size_w = as.factor(size_w)) %>%
+separated_data <- 
+  my_data %>%
+  mutate(plots_with_labels = str_replace(plots_with_labels, pattern = "S", replacement = "-S-")) %>%
+  mutate(plots_with_labels = str_replace(plots_with_labels, pattern = "M", replacement = "-M-")) %>%
+  mutate(plots_with_labels = str_replace(plots_with_labels, pattern = "L", replacement = "-L-")) %>%
+  separate(plots_with_labels, c("item", "size", "present"), sep = "-") 
+
+# plot data
+
+separated_data  %>%
+  mutate(size = as.factor(size)) %>%
+  mutate(present = as.factor(present)) %>%
   mutate(difference = my_rs - slider.response) %>%
-  filter(!is.na(size_w)) %>%
+  filter(!is.na(size)) %>%
   filter(!is.na(difference)) %>%
-  ggplot(aes(x = size_w, y = difference)) +
+  ggplot(aes(x = size:present, y = difference)) +
   geom_violin() +
   stat_summary(fun.data = mean_cl_boot)
 
-my_data %>%
-  mutate(size_w = as.factor(size_w)) %>%
+# calculate descriptives
+
+separated_data  %>%
+  mutate(size = as.factor(size)) %>%
+  mutate(present = as.factor(present)) %>%
   mutate(difference = my_rs - slider.response) %>%
-  filter(!is.na(size_w)) %>%
+  filter(!is.na(size)) %>%
   filter(!is.na(difference)) %>%
-  group_by(size_w) %>%
+  group_by(size, present) %>%
   summarise(mean = mean(difference))
 
-data_to_analyse <- my_data %>%
-  mutate(size_w = as.factor(size_w)) %>%
+# get data ready for analysis
+
+data_to_analyse <- separated_data  %>%
+  mutate(size = as.factor(size)) %>%
+  mutate(present = as.factor(present)) %>%
   mutate(difference = my_rs - slider.response) %>%
   filter(!is.na(size_w)) %>%
   filter(!is.na(difference)) 
 
-model <- aov_4(difference ~ size_w + (1 + size_w | participant), data = data_to_analyse)
+# build ANOVA
+
+model <- aov_4(difference ~ size * present + (1 + size * present | participant), data = data_to_analyse)
 summary(model)
 
-emmeans(model, pairwise ~ size_w)               
+emmeans(model, pairwise ~ size * present)               
